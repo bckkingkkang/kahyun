78ab12fd632add4fb312b48bdaa2a19471a7a970
gh.ko <gh.ko@renocomms.com>
2024-04-04 11:30:26 +0900

    servletFilter 사용 script문자열 치환

--- src/main/java/com/example/kahyun/KahyunApplication.java
+++ src/main/java/com/example/kahyun/KahyunApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
-@EnableAspectJAutoProxy
+@EnableAspectJAutoProxy	// Spring AOP 활성화
 @SpringBootApplication
 public class KahyunApplication {
 
